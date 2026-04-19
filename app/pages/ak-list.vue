ab01618250ebce3728e361ce8341d4e4d8a8c2d4
yagafarovrinat <yagafarovrinat@gmail.com>
2026-04-19 21:39:47 +0500

    Pravki

--- app/pages/ak-list.vue
+++ app/pages/ak-list.vue
@@ -6,7 +6,7 @@
 
 
     
-    <div>
+    <div class=" mx-1">
 
       <div class="p-4 border border-gray-500/30 rounded  flex justify-around">
         
@@ -42,7 +42,9 @@
             <!-- <div class="text-[10px]">{{ item.date }}</div> -->
           </div>
   
-          <div class="">{{ item.username }}</div>
+          <div class="">{{ item.username }}
+            <!-- <input type="text" :value="$(item.username)"> -->
+          </div>
           <div class="py-2 px-8 text-xl bg-gray-500/20">{{ item.id }}
             <!-- <div class=""></div> -->
           </div>
@@ -53,7 +55,7 @@
     </div>
     
     
-    <div class="my-4 flex justify-end gap-4 ">
+    <div class="my-4 mx-1 flex justify-end gap-4 ">
       
       <div class="p-4  bg-gray-500/20 hover:bg-blue-500/20 flex justify-center items-center hover:cursor-pointer" @click="handleEdit">
         <svg class="size-10" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" /></svg>
